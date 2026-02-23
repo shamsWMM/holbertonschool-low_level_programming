6a370cc9d824595c2a7c65e6f01f4a82c5667b7b
swatha <13080@holbertonstudents.com>
2026-02-23 13:07:16 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - find pointer to char c in s.
+ * @s: string to be checked.
+ * @c: char to look for.
+ *
+ * Return: pointer to char found or NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+
+	return (count);
+}
