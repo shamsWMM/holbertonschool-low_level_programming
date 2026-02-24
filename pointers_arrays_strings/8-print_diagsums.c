e068ca9353a601d2e1b1b9e01fc80b3fe2d12f64
swatha <13080@holbertonstudents.com>
2026-02-24 09:30:30 +0000

    diags

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,33 +1,45 @@
 #include "main.h"
 
+void print_digits(int d);
+
 /**
  * print_diagsums - prints diag sums.
  * @a: square matrix to be used.
  * @size: dimension of square matrix a.
  */
-
 void print_diagsums(int *a, int size)
 {
 	int i, d1 = 0, d2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		d1 += a[i][i];
-		d2 += a[i][size - i - 1];
+		d1 += *(a + i + i * size);
+		d2 += *(a + size - 1 - i + i * size);
 	}
 
-	while (d1)
-
+	print_digits(d1);
+	_putchar(',');
+	_putchar(' ');
+	print_digits(d2);
+	_putchar('\n');
 }
 
 /**
- * print_diagsums - prints diag sums.
- * @a: square matrix to be used.
- * @size: dimension of square matrix a.
+ * print_digits - prints number.
+ * @d: integer to be printed.
  */
 void print_digits(int d)
 {
+	if (d < 0)
+	{
+		_putchar('-');
+		d *= -1;
+	}
 	if (d / 10 == 0)
-		_putchar(d);
-	print_digits(d / 10);
+		_putchar('0' + d);
+	else
+	{
+		print_digits(d / 10);
+		_putchar('0' + d % 10);
+	}
 }
