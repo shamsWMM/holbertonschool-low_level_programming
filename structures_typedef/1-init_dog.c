3c3b693c2ccfba5b18227171bf727de00c10095f
shamsWMM <shamswathas@gmail.com>
2026-03-04 05:53:10 +0000

    fix dog init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,34 +12,7 @@ char *_strdup(char *s);
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = _strdup(name);
-d->owner = _strdup(owner);
+d->name = name; 
+d->owner = owner;
 d->age = age;
-}
-
-/**
- * _strdup - create a copy of s.
- * @s: the string to be copied.
- *
- * Return: a pointer to the copy of s, otherwise NULL.
- */
-char *_strdup(char *s)
-{
-char *result;
-size_t i, len = 0;
-
-if (s == NULL)
-	return (NULL);
-
-while (s[len])
-	len++;
-
-result = malloc(len + 1);
-if (result == NULL)
-	return (NULL);
-
-for (i = 0; i <= len; i++)
-	result[i] = s[i];
-
-return (result);
 }
